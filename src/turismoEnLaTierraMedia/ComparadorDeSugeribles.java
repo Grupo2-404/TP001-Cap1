c674eb859e0943982b2e794e72fa31090fca9278
AxelP-A <axelpablo_93@hotmail.com>
2021-10-03 12:57:43 -0300

    V8.1.1
     Se achica el comparator. Y se comienzan a cambiar los array de la clase
    Promoción por ArrayList.

--- src/turismoEnLaTierraMedia/ComparadorDeSugeribles.java
+++ src/turismoEnLaTierraMedia/ComparadorDeSugeribles.java
@@ -10,16 +10,19 @@ public class ComparadorDeSugeribles implements Comparator<Sugerible> {
 		this.preferida = tipo;
 	}
 	
+	/**
+	 * Comparamos si los tipos son iguales o diferentes, lo que nos interesa, es que la comprobación nos devuelva
+	 * un true o false, si la igualdad se cumple, comprobamos si los 2 sugeribles comprobados tienen la misma clase (ya 
+	 * sea atracción o promoción)si es el caso, comprobamos si los costos de visita son iguales, si son iguales, 
+	 * ordenamos en base al tiempo necesario, sino, ordenamos por el costo de visita. 
+	 *  En caso de que los tipos o las clases no sean iguales, las ordenamos en base al tipo o clase, según corresponda.
+	 *  
+	 */
 	@Override
 	public int compare(Sugerible sugerible, Sugerible otra) {
-
-		if (sugerible.getTipo() == preferida && otra.getTipo() == preferida) {
-			if (sugerible instanceof Promocion && otra instanceof Promocion) {
-				if (sugerible.getCostoDeVisita() == otra.getCostoDeVisita()) {
-					return (int) (sugerible.getTiempoNecesario() - otra.getTiempoNecesario()) * -1;
-				}
-				return (sugerible.getCostoDeVisita() - otra.getCostoDeVisita()) * -1;
-			} else if (sugerible instanceof Atraccion && otra instanceof Atraccion) {
+		
+		if ((sugerible.getTipo() == preferida) == (otra.getTipo() == preferida)){ 
+			if ((sugerible instanceof Promocion) == (otra instanceof Promocion)) {
 				if (sugerible.getCostoDeVisita() == otra.getCostoDeVisita()) {
 					return (int) (sugerible.getTiempoNecesario() - otra.getTiempoNecesario()) * -1;
 				}
@@ -29,7 +32,19 @@ public class ComparadorDeSugeribles implements Comparator<Sugerible> {
 			}
 			return -1;
 		}
-		if (sugerible.getTipo() != preferida && otra.getTipo() != preferida) {
+		if (sugerible.getTipo() != preferida){
+			return 1;
+		}
+		return -1;
+	}
+
+	/*
+	  public int compare(Sugerible sugerible, Sugerible otra) {
+		//if ((sugerible.getTipo() == preferida && otra.getTipo() == preferida) ||
+		// (sugerible.getTipo() != preferida && otra.getTipo() != preferida)){	Equivalente a línea 18.
+		
+		if ((sugerible.getTipo() == preferida) == (otra.getTipo() == preferida)){
+		
 			if (sugerible instanceof Promocion && otra instanceof Promocion) {
 				if (sugerible.getCostoDeVisita() == otra.getCostoDeVisita()) {
 					return (int) (sugerible.getTiempoNecesario() - otra.getTiempoNecesario()) * -1;
@@ -50,5 +65,6 @@ public class ComparadorDeSugeribles implements Comparator<Sugerible> {
 		}
 		return -1;
 	}
-
+	 */
+	
 }
